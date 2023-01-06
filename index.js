9f1b61e367ae8f4b764b9800a532e714f911d691
Palina Razhko <polina.r@dashbouquet.com>
2023-01-06 16:27:37 +0300

    fix: change email label

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function updatePerson(response) {
         {
           value: process.env.PERSON_EMAIL || "test@test.com",
           primary: true,
-          label: "test",
+          label: "from MVP Calc",
         },
       ],
     };
