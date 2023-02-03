cced514bb2d15a5d80c5766a0ddfcc324b3d8262
Palina Razhko <polina.r@dashbouquet.com>
2023-02-03 17:42:30 +0300

    feat: add LeadLabels

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ require("dotenv").config();
 //PERSON_NAME
 //DATA_BY_STEPS_VALUES
 
-const addDealClient = new pipedrive.ApiClient();
+const addLeadClient = new pipedrive.ApiClient();
 const addNotesToDealClient = new pipedrive.ApiClient();
 const addPersonClient = new pipedrive.ApiClient();
-addDealClient.authentications.api_key.apiKey = process.env.PIPEDRIVE_API_KEY;
+const addLeadLabelsClient = new pipedrive.ApiClient();
+addLeadClient.authentications.api_key.apiKey = process.env.PIPEDRIVE_API_KEY;
 addPersonClient.authentications.api_key.apiKey = process.env.PIPEDRIVE_API_KEY;
 addNotesToDealClient.authentications.api_key.apiKey =
   process.env.PIPEDRIVE_API_KEY;
+addLeadLabelsClient.authentications.api_key.apiKey =
+  process.env.PIPEDRIVE_API_KEY;
+
+const isWebForm = process.env.PERSON_NAME.includes("[WebForm]");
 
 async function addPerson() {
   try {
@@ -23,12 +28,15 @@ async function addPerson() {
     const api = new pipedrive.PersonsApi(addPersonClient);
 
     const data = {
-      name: process.env.PERSON_NAME || "Client Name Test",
+      name:
+        (isWebForm
+          ? process.env.PERSON_NAME.replace("[WebForm]", "")
+          : process.env.PERSON_NAME) || "Client Name Test",
       email: [
         {
           value: process.env.PERSON_EMAIL || "test@test.com",
           primary: true,
-          label: "from MVP Calc",
+          label: isWebForm ? "from WebForm" : "from MVP Calc",
         },
       ],
     };
@@ -45,19 +53,42 @@ async function addPerson() {
     console.log("Adding failed", errorToLog);
   }
 }
-async function addLead(response) {
+
+async function getLeadLabels() {
   try {
     console.log("Sending request...");
 
-    const api = new pipedrive.LeadsApi(addDealClient);
+    const api = new pipedrive.LeadLabelsApi(addLeadLabelsClient);
+
+    const getLeadLabelsResponse = await api.getLeadLabels();
+
+    console.log("Label to lead was added successfully!", getLeadLabelsResponse);
+    const labelID = getLeadLabelsResponse.data.filter((el) =>
+      isWebForm ? el.name == "WebForm" : el.name == "MVP"
+    )[0].id;
+    return labelID;
+  } catch (err) {
+    const errorToLog = err.context?.body || err;
+
+    console.log("Adding label to lead failed", errorToLog);
+  }
+}
+async function addLead(response) {
+  try {
+    console.log("Sending request...");
+    const labelId = await getLeadLabels();
+    const api = new pipedrive.LeadsApi(addLeadClient);
 
     const data = {
-      title: `${process.env.PERSON_EMAIL} MVP calc`,
+      title: isWebForm
+        ? process.env.PERSON_EMAIL
+        : `${process.env.PERSON_EMAIL} MVP calc`,
       person_id: response.data.id,
       value: {
         amount: +process.env.TOTAL_AMOUNT || 0,
         currency: "USD",
       },
+      label_ids: [labelId],
     };
     const leadResponse = await api.addLead(data);
 
@@ -72,6 +103,7 @@ async function addLead(response) {
     console.log("Adding lead failed", errorToLog);
   }
 }
+
 async function addNoteToDeal(response) {
   try {
     console.log("Sending request...");
